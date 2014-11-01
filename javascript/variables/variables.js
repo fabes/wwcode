affbc3cb3fb24e972b5fbe2aae793479f9ff5d27
Fabion Stephens <f.o.stephens@gmail.com>
2014-11-01 17:09:56 -0500

    Women Who Code Repo

---
+++ javascript/variables/variables.js
@@ -0,0 +1,21 @@
+/*
+What is a variable?
+	- A variable is a data item that stores a value.
+*/
+
+var first_name = "Fabion";
+var last_name = "Stephens";
+var age = 99;
+
+//How to print a variable to the screen
+// - document.write(first_name);
+
+//How to concatenate variables together
+/*
+	document.write(first_name+" "+last_name);
+	document.write("<br/>");
+	document.write("I am "+age+" years old.");
+*/
+
+//How to assign variable to another variable
+var full_name = first_name+" "+last_name;
